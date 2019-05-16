0fde5da9e924ad32ac08e32bef70c7759144b1ac
Ricardo Fredes <ricardofredes@MacBook-Pro-de-Ricardo.local>
2019-05-16 09:29:39 -0300

    add route for external api

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-const number = getRandomNumber(132)
-const api = `https://www.osvigaristas.com.br/charadas/pagina${number}.html`
-
 function fetchQuestion() {
-    return fetch(api)
+    const number = getRandomNumber(132)
+    const url = `https://www.osvigaristas.com.br/charadas/pagina${number}.html`
+    return fetch(url)
         .then(r => r.text())
         .then(r => {
             const text = r.replace(/[\n]/g ,'')
@@ -26,9 +25,17 @@ function splitQuestion(text){
     return {question,answer}
 }
 
+function fetchApi(isJson) {
+    const api = 'https://us-central1-kivson.cloudfunctions.net/charada-aleatoria'
+    const x = { headers: { "Accept": "application/json" } }
+    if (isJson) return fetch(api, x).then(r => r.json())
+    return fetch(api).then(r => r.text())
+}
+
 app.use(cors())
 
 app.get('/', (req, res) => fetchQuestion().then(r => res.send(r)))
-app.get('/charada', (req, res) => res.send('Charada'))
+app.get('/charada', (req, res) => fetchApi().then(r => res.send(r)))
+app.get('/charada/json', (req, res) => fetchApi(true).then(r => res.send(r)))
 
 module.exports = app
